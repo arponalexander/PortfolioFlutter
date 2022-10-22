e48786bbb5afc1867e6b66c4d06deb2abb2dd14e
arponalexander <arpon.alexander@gmail.com>
2022-10-23 06:34:12 +0800

    headline, and certificate overflowed

---
+++ lib/model/designProjects.dart
@@ -0,0 +1,13 @@
+class DesignProjects {
+  String from;
+  String to;
+  String description;
+  String title;
+  String programmingLanguage;
+  String database;
+  String logo;
+
+
+  DesignProjects(this.from, this.to, this.description, this.title,
+      this.programmingLanguage, this.database, this.logo);
+}
